1e1db9b4bb0984cd251ae8e8a2991fc5d1d38040
Hervé Frezza-Buet <Herve.Frezza-Buet@centralesupelec.fr>
2021-09-06 20:39:12 +0200

    updt

--- example/document.py
+++ example/document.py
@@ -22,18 +22,21 @@ col_computers, col_activities, col_dates = keys
 
 # Let us make a pie chart, counting the kinds of maintenance acts. We replace the acts by their full description, for nice legends.
 activities = data.decode_values(acts, col_activities, codes)
-document.make_pie(activities, col_activities, 'Summary of maintenance kinds ({} acts realized).')
+total = document.make_pie(activities, col_activities)
+document.caption('Summary of maintenance kinds ({} acts realized).'.format(total))
 
 # Let us make a pie chart as well, counting the location (cluster) of the maintenance.
 activities = data.group_values_dict(acts, col_computers, groups)
 activities = data.decode_values(activities, col_computers, codes)
-document.make_pie(activities, col_computers, 'Summary of maintenance location ({} acts realized).')
+total      = document.make_pie(activities, col_computers)
+document.caption('Summary of maintenance location ({} acts realized).'.format(total))
 
 # Let us make a bar chart, counting for each cluster the maintenance acts.
 activities = data.group_values_dict(acts,   col_computers, groups)
 activities = data.decode_values(activities, col_computers, codes)
 activities = data.decode_values(activities, col_activities, codes)
-document.make_bars(activities, col_computers, col_activities, 'Activities by cluster (({} acts realized).')
+total      = document.make_bars(activities, col_computers, col_activities)
+caption = 'Activities by cluster ({} acts realized).'.format(total)
 
 # We build the latex file for the report.
 document.generate()
